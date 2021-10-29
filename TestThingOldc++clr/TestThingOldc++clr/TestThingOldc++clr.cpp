8e2ab12155702e563845a4a572b3252db7b5298d
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-10-29 19:39:55 +0200

    Add files via upload

---
+++ TestThingOldc++clr/TestThingOldc++clr/TestThingOldc++clr.cpp
@@ -0,0 +1,16 @@
+// TestThingOldc++clr.cpp : main project file.
+
+#include "stdafx.h"
+#include <string>
+
+using namespace System;
+
+
+int main(array<System::String ^> ^args)
+{
+	System::String^ askaway;
+	askaway = Console::ReadLine();
+	Console::WriteLine("You Wrote - {0}", askaway);
+	Console::ReadLine();
+    return 0;
+}
